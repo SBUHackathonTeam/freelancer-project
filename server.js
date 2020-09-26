ff73da7170348058780d288802976096ba6bf8f0
altBrianBrennglass <47303908+altBrianBrennglass@users.noreply.github.com>
2020-09-25 21:59:27 -0400

    added database

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const loginBusiness = require('./APIs/loginBusiness');
+const mongoose = require('mongoose');
+
+mongoose.connect(`mongodb+srv://richrich:Admin123@cluster0.lua7i.mongodb.net/<dbname>?retryWrites=true&w=majority`,
+                 {
+                    useNewUrlParser: true, 
+                    useUnifiedTopology: true,
+                    useFindAndModify: false
+                }
+)
+.then(console.log('connected'))
+.catch((err)=>console.log('\n\nmongodb connection failed\n\n'))
 
 const app = express();
 
 app.use('/', express.static('public'));
+/*
 app.use('/loginFreelancer');
-app.use('/loginBusiness');
+app.use('/loginBusiness', loginBusiness);
 app.use('/registerFreelancer');
 app.use('/registerBusiness');
 app.use('/getProfiles');
+*/
 app.listen(8080, ()=>console.log('listening on port 8080'));
