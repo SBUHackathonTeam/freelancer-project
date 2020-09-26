68fc0ac75fd40964754789c025949ef7a96578fc
altBrianBrennglass <47303908+altBrianBrennglass@users.noreply.github.com>
2020-09-26 02:58:40 -0400

    working photos

--- server.js
+++ server.js
@@ -1,25 +1,24 @@
 const express = require('express');
-const loginBusiness = require('./APIs/loginBusiness');
+const login = require('./apis/login');
+const profilePage = require('./apis/profilePage');
 const mongoose = require('mongoose');
 
-mongoose.connect(`mongodb+srv://richrich:Admin123@cluster0.lua7i.mongodb.net/<dbname>?retryWrites=true&w=majority`,
-                 {
-                    useNewUrlParser: true, 
-                    useUnifiedTopology: true,
-                    useFindAndModify: false
-                }
-)
-.then(console.log('connected'))
-.catch((err)=>console.log('\n\nmongodb connection failed\n\n'))
+
+mongoose.connect('mongodb+srv://richrich:Admin123@cluster0.lua7i.mongodb.net/freelancer?retryWrites=true&w=majority',
+{
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}).then(res=>console.log('connected'))
+.catch(err=>console.log(err));
+                
+
 
 const app = express();
 
 app.use('/', express.static('public'));
-/*
-app.use('/loginFreelancer');
-app.use('/loginBusiness', loginBusiness);
-app.use('/registerFreelancer');
-app.use('/registerBusiness');
-app.use('/getProfiles');
-*/
-app.listen(8080, ()=>console.log('listening on port 8080'));
+app.use(express.json());
+app.use('/login', login);
+app.use('/profilePage', profilePage);
+
+app.listen(8080, ()=>console.log('server on'));
